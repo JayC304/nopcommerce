a452d56cbd823117e1591fe9037a910cb961fafe
AnhTran <ttanh30dh@gmail.com>
2023-08-15 13:30:22 +0700

    update

---
+++ actions/pageObjects/HomePageObject.java
@@ -0,0 +1,9 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+
+public class HomePageObject extends BasePage {
+	private WebDriver driver;
+}
---
+++ interfaces/pageUIs/HomePageUI.java
@@ -0,0 +1,5 @@
+package pageUIs;
+
+public class HomePageUI {
+	public static String resgisterbtn;
+}
